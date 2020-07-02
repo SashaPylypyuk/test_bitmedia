e83a5da05acb4f9132c5c01cd159700b600f961e
SahaPylypyuk <sashapulupyk@gmail.com>
2020-07-02 15:44:00 +0300

    Updates

---
+++ precache-manifest.c5334d059c661c284b28d6796e79b33e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f15fec17e3e7fc61c62197c3bc0641",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63de8e98f3503c55e0f8",
+    "url": "./static/css/main.fe6e0af5.chunk.css"
+  },
+  {
+    "revision": "697bfcf6fc9061937c00",
+    "url": "./static/js/2.f52a8be5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f52a8be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63de8e98f3503c55e0f8",
+    "url": "./static/js/main.504960af.chunk.js"
+  },
+  {
+    "revision": "d578208433d8114d36da",
+    "url": "./static/js/runtime-main.ad80f0b6.js"
+  },
+  {
+    "revision": "2969c024b1a676355262c7011274b47c",
+    "url": "./static/media/Group 13.2969c024.svg"
+  },
+  {
+    "revision": "f751d5fdbe4c30f473d5749e32afc888",
+    "url": "./static/media/Group 14.f751d5fd.svg"
+  },
+  {
+    "revision": "5f21ae72f7cf7cec8a0f76943f925c89",
+    "url": "./static/media/Group 15.5f21ae72.svg"
+  },
+  {
+    "revision": "105ae180f18cca9bf8b98cb6fd6df3c3",
+    "url": "./static/media/Rectangle 8.105ae180.svg"
+  },
+  {
+    "revision": "be25a26c07bdac07a1b75717e12c27f1",
+    "url": "./static/media/footer.be25a26c.svg"
+  },
+  {
+    "revision": "47d7e2e8a725f73e2c2f91ce472d6146",
+    "url": "./static/media/mobile.47d7e2e8.png"
+  }
+]);
